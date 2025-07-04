59aba10c9032f1e60235530e18048fdb101f56ba
martins-samuel <ead@academico.local>
2025-07-04 16:23:58 -0300

    Banco de dados atividade

---
+++ atividades/atividade_10/main.py
@@ -0,0 +1,7 @@
+'''Crie um programa que exiba um menu com as seguintes operações:
+-Cadastrar um novo usuario no banco 
+-Listar todos os usuarios do banco 
+-Sair do programa 
+# NOTE  a entidade deve ser gerada pel python 
+# NOTE - nome do banco db_atividade_10. Entidade Usuario. atributos:
+-Nome;-CPF; E-mail; Data de nascimento; Telefone; Profissão; Endereço; Altura; Peso'''
--- banco_de_dados/01_banco_parte_01/main.py
+++ banco_de_dados/01_banco_parte_01/main.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
+import os 
 
+def limpar():
+    os.system("cls" if os.name == "nt" else "clear")
 try:
     engine = create_engine("sqlite:///meu_primeiro_banco.db")
     Base = declarative_base()
@@ -9,44 +13,59 @@ try:
     class Pessoa(Base):
         __tablename__ = "pessoa"
         
-        # Atributos 
         id_pessoa = Column(Integer, primary_key=True, autoincrement=True)
         nome = Column(String, nullable=False)
-        # FIXME data_nascimento = Column(Date, nullable=False)
+        data_nasc = Column(Date, nullable=False)
         email = Column(String, nullable=False, unique=True)
         cpf = Column(String, nullable=False, unique=True)
         genero = Column(String, nullable=True)
         tipo_sanguineo = Column(String, nullable=True)
-        #cria a tabela
+
     Base.metadata.create_all(engine)
-    print("Entidade Pessoa criada com sucesso")
+    #print("Entidade Pessoa criada com sucesso")"No futuro deletar"
 except Exception as e:
     print(f"Erro no sistema. {e}")
+
 try: 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    #cadastrar usuário no banco 
+    # Entrada de dados do usuário
     nome = input("Informe o nome: ").strip().title()
-    #data_nasc = input("Informe a data de nascimento: (aaaa-mm-dd) ").strip().lower()
-    email = input("Informe o email ").strip().lower()
-    cpf = input("Informe o cpf ").strip()
+    data_nasc_str = input("Informe a data de nascimento: (dd/mm/aaaa) ").strip()
+    data_nasc = datetime.strptime(data_nasc_str, "%d/%m/%Y").date()
+    email = input("Informe o email: ").strip().lower()
+    cpf = input("Informe o cpf: ").strip()
     genero = input("Informe o gênero: ").strip()
-    tipo_sanguineo = input("Informe o tipo sanguineo : ").strip().upper()
+    tipo_sanguineo = input("Informe o tipo sanguíneo: ").strip().upper()
 
+    # Cria nova pessoa
     nova_pessoa = Pessoa(
-        nome = nome,
-        #data_nasc = data_nasc, 
-        email = email, 
-        cpf = cpf,
-        genero = genero,
-        tipo_sanguineo = tipo_sanguineo
+        nome=nome,
+        data_nasc=data_nasc,
+        email=email,
+        cpf=cpf,
+        genero=genero,
+        tipo_sanguineo=tipo_sanguineo
     )
 
+    # Adiciona e confirma no banco
     session.add(nova_pessoa)
     session.commit()
     print("Pessoa cadastrada com sucesso")
 
-    session.close()
+    #deletar no futuro session.close()
 except Exception as e:
-    print(f"Não foi possível cadastrar novo usuário {e}")
+    print(f"Não foi possível cadastrar novo usuário: {e}")
+
+pessoas = session.query(Pessoa).all()
+print(f"\n{'-'*20} Pessoas Cadastradas {'-' * 20}")
+for pessoa in pessoas:
+    print(f"Id: {pessoa.id_pessoa}")
+    print(f"Nome: {pessoa.nome}")
+    print(f"Data de nascimento: {pessoa.data_nasc.strftime('%d/%m/%Y')}")
+    print(f"E-mail: {pessoa.email}")
+    print(f"Gênero: {pessoa.genero}")
+    print(f"Tipo sanguíneo: {pessoa.tipo_sanguineo}")
+    print(f"{'-' * 60}")
+session.close()
